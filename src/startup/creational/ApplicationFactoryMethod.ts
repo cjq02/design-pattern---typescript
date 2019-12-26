d448a44c1741a51d64c205ff2c5514c3c1764d84
jq_chen <jq_chen@onlyou.com>
2019-12-26 11:34:20 +0800

    Factory Method Pattern

--- src/startup/creational/ApplicationFactoryMethod.ts
+++ src/startup/creational/ApplicationFactoryMethod.ts
@@ -3,15 +3,19 @@ import OrderFactory from "@patterns/creational/factory method/OrderFactory";
 import { Application } from "@annotation/Application";
 import { Autowired } from "@annotation/Autowired";
 import _ = require("lodash");
-import ReadlineUtils from "@utils/ReadlineUtils";
 import PrintUtils from "@utils/PrintUtils";
 import { OrderEnum, OrderEnumRef } from "@patterns/creational/factory method/enum/OrderEnum";
+import MultiChoiceProcess from "@process/MultiChoiceProcess";
+import Constants from "@common/Constants";
 
 @Application.Startup
 class ApplicationFactoryMethod {
 
     @Autowired(OrderFactory)
     private static orderFactory: OrderFactory;
+
+    @Autowired(MultiChoiceProcess)
+    private static process: MultiChoiceProcess;
 
     @Application.Autostart
     public static main(): void {
@@ -21,9 +25,12 @@ class ApplicationFactoryMethod {
     static choose(): void {
         PrintUtils.printChooseMsg(OrderEnum, OrderEnumRef);
 
-        ReadlineUtils.question((answer: any) => {
+        this.process.question((answer: any) => {
+            if (answer === Constants.EXIT) {
+                return this.process.terminate();
+            }
+
             this.orderFactory.getOrder(answer).add();
-            return true;
         })
     }
 
